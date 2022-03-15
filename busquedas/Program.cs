3450a647ad1a5826bf94f4b9dfb16507d5e89710
Israel Sanchez <ishisra14@hotmail.com>
2022-03-15 13:14:43 -0600

    BFS complete!

---
+++ busquedas/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+    15-03-2022
+    Rodrigo S. de la Rosa Andres
+    Israel Sanchez Hinojosa
+ 
+    Programa para mostrar los algoritmos de busquedas en grafos (anchura, profundidad y profundidad limitada)
+*/
+
+
+using System;
+
+namespace busquedas
+{
+    class Program {
+        static void Main(string[] args){
+            Grafo g = new Grafo(10);
+
+            g.agregarVertice(0, 1);
+            g.agregarVertice(0, 2);
+            g.agregarVertice(1, 3);
+            g.agregarVertice(2, 4);
+            g.agregarVertice(2, 5);
+            g.agregarVertice(4, 5);
+            g.agregarVertice(3, 6);
+            g.agregarVertice(4, 6);
+            g.agregarVertice(4, 7);
+            g.agregarVertice(7, 8);
+            g.agregarVertice(7, 9);
+            g.agregarVertice(8, 9);
+
+
+            Console.Write("Breadth First Search (BFS)\n");
+            Recorridos recorrido = new Recorridos(g.getSize());
+            recorrido.BFS(2, g.getAdj());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ busquedas/Recorridos.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace busquedas
+{
+    class Recorridos{
+        private int _v;
+        public Recorridos(int v) {
+            _v = v;
+        }
+
+        public void BFS(int inicio, LinkedList<int>[] _adj) {
+
+            // Marcar que los vertices no han sido visitados. 
+            bool[] isVisited = new bool[_v];
+            for (int i = 0; i < _v; i++)
+                isVisited[i] = false;
+
+            LinkedList<int> queue = new LinkedList<int>();  // Queue para el BFS
+
+            isVisited[inicio] = true;
+            queue.AddLast(inicio);
+
+            while (queue.Any()) {
+                inicio = queue.First();
+                Console.Write(inicio + " ");
+                queue.RemoveFirst();
+
+                LinkedList<int> lista = _adj[inicio];
+
+                foreach (var elemento in lista) {
+                    if (!isVisited[elemento]) {
+                        isVisited[elemento] = true;
+                        queue.AddLast(elemento);
+                    }
+                }
+
+            }
+
+            Console.WriteLine("\n\n");
+        }
+    }
+}
